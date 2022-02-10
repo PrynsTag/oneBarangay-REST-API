150fece699a8cd5c4fa87ed760ca2892d46ab2f1
Restyled.io <commits@restyled.io>
2022-02-10 03:42:16 +0000

    Restyled by black

--- onebarangay_psql/announcement/migrations/0002_alter_announcement_options_announcement_slug_and_more.py
+++ onebarangay_psql/announcement/migrations/0002_alter_announcement_options_announcement_slug_and_more.py
@@ -6,28 +6,33 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('announcement', '0001_initial'),
+        ("announcement", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='announcement',
-            options={'ordering': ['-created_at']},
+            name="announcement",
+            options={"ordering": ["-created_at"]},
         ),
         migrations.AddField(
-            model_name='announcement',
-            name='slug',
-            field=models.SlugField(default='test-announcement', max_length=255, unique=True),
+            model_name="announcement",
+            name="slug",
+            field=models.SlugField(
+                default="test-announcement", max_length=255, unique=True
+            ),
             preserve_default=False,
         ),
         migrations.AlterField(
-            model_name='announcement',
-            name='thumbnail',
-            field=models.ImageField(default='announcement/thumbnail/default.jpg', upload_to='announcement/thumbnail'),
+            model_name="announcement",
+            name="thumbnail",
+            field=models.ImageField(
+                default="announcement/thumbnail/default.jpg",
+                upload_to="announcement/thumbnail",
+            ),
         ),
         migrations.AlterField(
-            model_name='announcement',
-            name='title',
+            model_name="announcement",
+            name="title",
             field=models.CharField(max_length=255, unique=True),
         ),
     ]
