2083fd5ec7d0858d1e518a9ab09e90174f4da2ac
PrynsTag <princevelasco16@gmail.com>
2022-02-14 13:02:44 +0800

    feat[appointment]: initialize appointment files

---
+++ onebarangay_psql/appointment/apps.py
@@ -0,0 +1,9 @@
+"""Create your appointment apps here."""
+from django.apps import AppConfig
+
+
+class AppointmentConfig(AppConfig):
+    """AppointmentConfig class."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "onebarangay_psql.appointment"
