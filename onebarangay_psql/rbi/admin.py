6f4b5d2899ad2161ccd6dd1236dfd1468edcebca
PrynsTag <princevelasco16@gmail.com>
2022-02-19 10:14:48 +0800

    feat[rbi]: register models in admin.py

--- onebarangay_psql/rbi/admin.py
+++ onebarangay_psql/rbi/admin.py
@@ -1,1 +1,7 @@
 """Register your rbi models here."""
+from django.contrib import admin
+
+from onebarangay_psql.rbi import models
+
+admin.site.register(models.HouseRecord)
+admin.site.register(models.FamilyMember)
