10499c7e647910a14cfc70eb641a2b420a12d4fe
PrynsTag <princevelasco16@gmail.com>
2022-02-20 09:44:04 +0800

    refactor[rbi]: added model serializers to HouseRecord and FamilyMember

--- onebarangay_psql/rbi/serializers.py
+++ onebarangay_psql/rbi/serializers.py
@@ -1,1 +1,29 @@
 """Create your rbi serializers here."""
+from rest_framework import serializers
+
+from onebarangay_psql.rbi.models import FamilyMember, HouseRecord
+from onebarangay_psql.utils.choice_field import ChoicesField
+
+
+class HouseRecordSerializer(serializers.ModelSerializer):
+    """House record serializer."""
+
+    class Meta:
+        """Meta class for HouseRecordSerializer."""
+
+        model = HouseRecord
+        fields = "__all__"
+
+
+class FamilyMemberSerializer(serializers.ModelSerializer):
+    """Serializer for FamilyMember model."""
+
+    house_record = HouseRecordSerializer
+    civil_status = ChoicesField(choices=FamilyMember.CivilStatus.choices)
+    gender = ChoicesField(choices=FamilyMember.Gender.choices)
+
+    class Meta:
+        """Meta class for FamilyMemberSerializer."""
+
+        model = FamilyMember
+        fields = "__all__"
