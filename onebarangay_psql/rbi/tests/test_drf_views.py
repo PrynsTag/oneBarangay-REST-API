cc1f6310fd0433de05664078bb578d139ed4a0ff
PrynsTag <princevelasco16@gmail.com>
2022-02-20 10:36:27 +0800

    refactor[rbi]: add viewset.py and tests to HouseRecord and FamilyMember model and serializer

--- onebarangay_psql/rbi/tests/test_drf_views.py
+++ onebarangay_psql/rbi/tests/test_drf_views.py
@@ -1,1 +1,142 @@
-"""Create your rbi drf views test here."""
+"""Create your rbi drf viewsets test here."""
+import pytest
+from django.test import RequestFactory
+from django.urls import reverse
+from rest_framework.permissions import IsAdminUser
+from rest_framework.request import Request
+
+from onebarangay_psql.rbi.models import FamilyMember, HouseRecord
+from onebarangay_psql.rbi.serializers import (
+    FamilyMemberSerializer,
+    HouseRecordSerializer,
+)
+from onebarangay_psql.rbi.viewset import FamilyMemberViewSet, HouseRecordViewSet
+from onebarangay_psql.users.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+class TestHouseRecordViewSet:
+    """Test HouseRecordViewSet."""
+
+    def test_get_queryset(
+        self, admin_user: User, house_record: HouseRecord, rf: RequestFactory
+    ):
+        """Test get_queryset returns a house record queryset.
+
+        Scenario:
+            - The get_queryset method should return a queryset of created house records.
+        Args:
+            admin_user (User): The admin user making the get request.
+            house_record (HouseRecord): The house record to be returned.
+            rf (RequestFactory): The request factory mocking the get request.
+        """
+        viewset = HouseRecordViewSet()
+
+        request = rf.get(reverse("api:house-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+
+        assert house_record in viewset.get_queryset()
+
+    def test_get_serializer(self, admin_user: User, rf: RequestFactory):
+        """Test get_serializer returns a HouseRecordSerializer.
+
+        Scenario:
+            - The get_serializer method should return an instance of HouseRecordSerializer.
+        Args:
+            admin_user (User): The admin user making the get request.
+            rf (RequestFactory): The request factory mocking the get request.
+        """
+        viewset = HouseRecordViewSet()
+
+        request = rf.get(reverse("api:house-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+        viewset.format_kwarg = None
+
+        assert isinstance(viewset.get_serializer(), HouseRecordSerializer)
+
+    def test_get_permission(self, admin_user: User, rf: RequestFactory):
+        """Test get_permission returns IsAdminUser.
+
+        Scenario:
+            - The get_permission method should return an instance of IsAdminUser.
+        Args:
+            admin_user (User): The admin user making the get request.
+            rf (RequestFactory): The request factory mocking the get request.
+        """
+        viewset = HouseRecordViewSet()
+
+        request = rf.get(reverse("api:house-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+        viewset.format_kwarg = None
+
+        assert isinstance(viewset.get_permissions()[0], IsAdminUser)
+
+
+class TestFamilyMemberViewSet:
+    """Test FamilyMemberViewSet."""
+
+    def test_get_queryset(
+        self, admin_user: User, family_member: FamilyMember, rf: RequestFactory
+    ):
+        """Test get_queryset returns a family member queryset.
+
+        Scenario:
+            - The get_queryset method should return a queryset of created family members.
+        Args:
+            admin_user (User): The admin user making the get request.
+            family_member (FamilyMember): The family member to be returned.
+            rf (RequestFactory): The request factory to mock get request.
+        """
+        viewset = FamilyMemberViewSet()
+
+        request = rf.get(reverse("api:family-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+
+        assert family_member in viewset.get_queryset()
+
+    def test_get_serializer(self, admin_user: User, rf: RequestFactory):
+        """Test get_serializer returns an instance of FamilyMemberSerializer.
+
+        Scenario:
+            - The get_serializer method should return an instance of FamilyMemberSerializer.
+        Args:
+            admin_user (User): The admin user making the get request.
+            rf (RequestFactory): The request factory to mock get request.
+        """
+        viewset = FamilyMemberViewSet()
+
+        request = rf.get(reverse("api:family-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+        viewset.format_kwarg = None
+
+        assert isinstance(viewset.get_serializer(), FamilyMemberSerializer)
+
+    def test_get_permission(self, admin_user: User, rf: RequestFactory):
+        """Test get_permission returns an instance of IsAdminUser.
+
+        Scenario:
+            - The get_permission method should return an instance of IsAdminUser.
+        Args:
+            admin_user (User): The admin user making the get request.
+            rf (RequestFactory): The request factory to mock get request.
+        """
+        viewset = FamilyMemberViewSet()
+
+        request = rf.get(reverse("api:family-list"))
+        request.user = admin_user
+
+        viewset.request = Request(request)
+        viewset.format_kwarg = None
+
+        assert isinstance(viewset.get_permissions()[0], IsAdminUser)
--- onebarangay_psql/rbi/viewset.py
+++ onebarangay_psql/rbi/viewset.py
@@ -1,1 +1,25 @@
 """Create your rbi viewsets here."""
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
+from onebarangay_psql.rbi.models import FamilyMember, HouseRecord
+from onebarangay_psql.rbi.serializers import (
+    FamilyMemberSerializer,
+    HouseRecordSerializer,
+)
+
+
+class HouseRecordViewSet(viewsets.ModelViewSet):
+    """HouseRecord viewset."""
+
+    queryset = HouseRecord.objects.all()
+    serializer_class = HouseRecordSerializer
+    permission_classes = [IsAdminUser]
+
+
+class FamilyMemberViewSet(viewsets.ModelViewSet):
+    """FamilyMember viewset."""
+
+    queryset = FamilyMember.objects.all()
+    serializer_class = FamilyMemberSerializer
+    permission_classes = [IsAdminUser]
