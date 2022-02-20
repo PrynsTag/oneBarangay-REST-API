a9e81f8c6e36e4e2f49fa8a97cf9a8e55a504395
PrynsTag <princevelasco16@gmail.com>
2022-02-20 11:27:00 +0800

    feat[rbi]: added a custom choice field for representing human-readable choices

---
+++ onebarangay_psql/utils/choice_field.py
@@ -0,0 +1,10 @@
+"""Create your custom choice field here."""
+from rest_framework import serializers
+
+
+class ChoicesField(serializers.ChoiceField):
+    """Custom ChoiceField to handle human-readable choices."""
+
+    def to_representation(self, value):
+        """Return the human-readable choice."""
+        return self.choices[value]
