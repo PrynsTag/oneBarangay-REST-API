d7d140a8e892e73856557efa64ba53317ccacfd7
PrynsTag <princevelasco16@gmail.com>
2022-02-23 10:34:46 +0800

    feat[statistics]: add function to refresh materialized views

---
+++ onebarangay_psql/utils/refresh_material_view.py
@@ -0,0 +1,31 @@
+"""Refreshes Material Views."""
+import datetime
+import time
+
+from django.db import connection
+
+from onebarangay_psql.statistics.models import RefreshMaterializedView
+
+
+def refresh_mv():
+    """Refresh Materialized Views in the Database."""
+    start_time = time.monotonic()
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_total;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_user_signup;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_appointment;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_user_login;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_age_group;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_citizenship;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_civil_status;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_average;
+        REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_statistics_social_class;
+        """
+        )
+    end_time = time.monotonic()
+
+    RefreshMaterializedView.objects.create(
+        duration=datetime.timedelta(seconds=end_time - start_time)
+    )
