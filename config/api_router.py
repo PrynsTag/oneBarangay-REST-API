27028b38aa6f35e419302583a4e241588529bfe6
PrynsTag <princevelasco16@gmail.com>
2022-02-23 10:31:10 +0800

    feat[statistics]: added api routes and tests for viewsets.py

--- config/api_router.py
+++ config/api_router.py
@@ -5,6 +5,7 @@ from rest_framework.routers import BaseRouter, DefaultRouter, SimpleRouter
 from onebarangay_psql.announcement.viewset import AnnouncementViewSet
 from onebarangay_psql.appointment.viewset import AppointmentViewSet, StatusUpdateViewSet
 from onebarangay_psql.rbi.viewset import FamilyMemberViewSet, HouseRecordViewSet
+from onebarangay_psql.statistics import viewset
 from onebarangay_psql.users.api.views import (
     UserListViewSet,
     UserProfileListViewSet,
@@ -28,6 +29,48 @@ router.register("appointment", AppointmentViewSet)
 router.register("status", StatusUpdateViewSet, basename="status")
 router.register("house-record", HouseRecordViewSet, basename="house")
 router.register("family-member", FamilyMemberViewSet, basename="family")
+router.register(
+    "statistics/user-signup",
+    viewset.UserSignUpMaterializedViewSet,
+    basename="mv-signup",
+)
+router.register(
+    "statistics/totals", viewset.TotalMaterializedViewSet, basename="mv-total"
+)
+router.register(
+    "statistics/appointment",
+    viewset.AppointmentMaterializedViewSet,
+    basename="mv-appointment",
+)
+router.register(
+    "statistics/user-login", viewset.UserLogInMaterializedViewSet, basename="mv-signin"
+)
+router.register(
+    "statistics/age-group", viewset.AgeGroupMaterializedViewSet, basename="mv-age-group"
+)
+router.register(
+    "statistics/citizenship",
+    viewset.CitizenshipMaterializedViewSet,
+    basename="mv-citizenship",
+)
+router.register(
+    "statistics/civil-status",
+    viewset.CivilStatusMaterializedViewSet,
+    basename="mv-civil-status",
+)
+router.register(
+    "statistics/average", viewset.AverageMaterializedViewSet, basename="mv-average"
+)
+router.register(
+    "statistics/social-class",
+    viewset.SocialClassMaterializedViewSet,
+    basename="mv-social-class",
+)
+router.register(
+    "statistics/refresh",
+    viewset.RefreshMaterialViewSet,
+    basename="mv-refresh",
+)
 
 app_name = "api"
 urlpatterns = router.urls
--- onebarangay_psql/statistics/tests/test_drf_urls.py
+++ onebarangay_psql/statistics/tests/test_drf_urls.py
@@ -1,1 +1,27 @@
 """Create your statistics drf urls tests here."""
+import pytest
+from django.urls import resolve, reverse
+
+
+class TestStatisticsViewSetUrls:
+    """Test StatisticsViewSet urls."""
+
+    @pytest.mark.parametrize(
+        ("app_url", "url_path"),
+        [
+            ("api:mv-signup-list", "/api/statistics/user-signup/"),
+            ("api:mv-total-list", "/api/statistics/totals/"),
+            ("api:mv-appointment-list", "/api/statistics/appointment/"),
+            ("api:mv-signin-list", "/api/statistics/user-login/"),
+            ("api:mv-age-group-list", "/api/statistics/age-group/"),
+            ("api:mv-citizenship-list", "/api/statistics/citizenship/"),
+            ("api:mv-civil-status-list", "/api/statistics/civil-status/"),
+            ("api:mv-average-list", "/api/statistics/average/"),
+            ("api:mv-social-class-list", "/api/statistics/social-class/"),
+            ("api:mv-refresh-list", "/api/statistics/refresh/"),
+        ],
+    )
+    def test_statistics_list(self, app_url: str, url_path: str):
+        """Test statistics list url."""
+        assert reverse(app_url) == url_path
+        assert resolve(url_path).view_name == app_url
