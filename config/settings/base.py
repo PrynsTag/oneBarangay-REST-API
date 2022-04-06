17a0768b9d2743fc046830212703f14bb48b9816
PrynsTag <princevelasco16@gmail.com>
2022-04-06 20:23:39 +0800

    feat[annnouncement]: add signal to create notification

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,7 +8,7 @@ ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = ROOT_DIR / "onebarangay_psql"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(ROOT_DIR / ".env"))
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
     "import_export",
     "auditlog",
     "tinymce",
+    "push_notifications",
 ]
 
 LOCAL_APPS = [
@@ -349,4 +350,13 @@ SOCIALACCOUNT_PROVIDERS = {
             "access_type": "online",
         },
     },
+}
+
+
+# django-push-notifications
+# ------------------------------------------------------------------------------
+# https://github.com/jazzband/django-push-notifications#settings-list
+PUSH_NOTIFICATIONS_SETTINGS = {
+    "FCM_API_KEY": env.str("FCM_API_KEY"),
+    "UPDATE_ON_DUPLICATE_REG_ID": True,
 }
--- onebarangay_psql/announcement/apps.py
+++ onebarangay_psql/announcement/apps.py
@@ -1,4 +1,6 @@
 """Create your app config for announcement apps."""
+import contextlib
+
 from django.apps import AppConfig
 
 
@@ -7,3 +9,9 @@ class AnnouncementConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "onebarangay_psql.announcement"
+
+    def ready(self):
+        """Import signal handlers."""
+        # pylint: disable=C0415,W0611
+        with contextlib.suppress(ImportError):
+            import onebarangay_psql.announcement.signals  # noqa: F401
