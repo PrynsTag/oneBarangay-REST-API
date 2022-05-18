c2210cfdf4c84bdcf528877e4db8546adc45610d
PrynsTag <princevelasco16@gmail.com>
2022-05-18 19:47:50 +0800

    refactor[users]: remove test for adding user in django admin

--- onebarangay_psql/users/tests/test_admin.py
+++ onebarangay_psql/users/tests/test_admin.py
@@ -1,10 +1,10 @@
 """Create your tests for the admin app here."""
 import pytest
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from onebarangay_psql.users.models import User
-
 pytestmark = pytest.mark.django_db
+User = get_user_model()
 
 
 class TestUserAdmin:
@@ -27,17 +27,6 @@ class TestUserAdmin:
         url = reverse("admin:users_user_add")
         response = admin_client.get(url)
         assert response.status_code == 200
-
-        response = admin_client.post(
-            url,
-            data={
-                "username": "test",
-                "password1": "My_R@ndom-P@ssw0rd",
-                "password2": "My_R@ndom-P@ssw0rd",
-            },
-        )
-        assert response.status_code == 302
-        assert User.objects.filter(username="test").exists()
 
     def test_view_user(self, admin_client):
         """Test the view user functionality."""
