066b489f2baa1f8b59352c96a2663860e97a2852
Prince Carl Velasco <github@princevelasco.com>
2022-11-24 16:15:24 +0800

    [refactor]: Used gettext instead of gettext_lazy for fixing type checking

--- onebarangay_psql/users/admin.py
+++ onebarangay_psql/users/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
